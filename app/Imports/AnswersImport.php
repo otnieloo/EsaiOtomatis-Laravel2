0b53070946d69812bd7f0ffc7a685f327085ee66
otnieloo <167006038@gmail.com>
2021-01-23 15:42:48 +0700

    bug csv

--- app/Imports/AnswersImport.php
+++ app/Imports/AnswersImport.php
@@ -49,96 +49,97 @@ class AnswersImport implements ToCollection
             $jawaban_siswa[] = $c[3];
             $jawaban_siswa[] = $c[4];
             $jawaban_siswa[] = $c[5];
+            dump($jawaban_siswa);
             // Submit isi ujian
-            $ujian = Exam::where('id_ujian',$this->id_ujian)->first();
-            $question = Question::where('id_ujian',$this->id_ujian)->get();
-            if(isset($ujian)){
-                $count = 0;
-                $total_nilai = array();
-                $total_nilaiqe = array();
-                foreach($jawaban_siswa as $j){
-                    try{
-                        $id_soal = $question[$count]->id_soal;
-                        $data = [
-                            'id_soal' => $id_soal,
-                            'id_siswa' => $id_siswa,
-                            'id_ujian' => $ujian->id_ujian,
-                            'jawaban' => $j
-                        ];
+            // $ujian = Exam::where('id_ujian',$this->id_ujian)->first();
+            // $question = Question::where('id_ujian',$this->id_ujian)->get();
+            // if(isset($ujian)){
+            //     $count = 0;
+            //     $total_nilai = array();
+            //     $total_nilaiqe = array();
+            //     foreach($jawaban_siswa as $j){
+            //         try{
+            //             $id_soal = $question[$count]->id_soal;
+            //             $data = [
+            //                 'id_soal' => $id_soal,
+            //                 'id_siswa' => $id_siswa,
+            //                 'id_ujian' => $ujian->id_ujian,
+            //                 'jawaban' => $j
+            //             ];
 
-                        $ans = Answer::create($data);
-                        if($ans){
-                            echo 'answer ok, ';
-                        }
-                        // Submit similaritas
-                        $similaritas = $this->cekSimilaritas($id_soal,$j);
-                        $jawaban = Answer::where('id_soal',$id_soal)
-                                        ->where('id_siswa',$id_siswa)
-                                        ->first();
-                        if(isset($similaritas)){
-                            if($similaritas->cosine1 >= 0.7){
-                                $nilai_konversi = 2;
-                            }else if($similaritas->cosine1 >= 0.4){
-                                $nilai_konversi = 1;
-                            }else if($similaritas->cosine1 >= 0.1){
-                                $nilai_konversi = 0.5;
-                            }else{
-                                $nilai_konversi = 0;
-                            }
+            //             $ans = Answer::create($data);
+            //             if($ans){
+            //                 echo 'answer ok, ';
+            //             }
+            //             // Submit similaritas
+            //             $similaritas = $this->cekSimilaritas($id_soal,$j);
+            //             $jawaban = Answer::where('id_soal',$id_soal)
+            //                             ->where('id_siswa',$id_siswa)
+            //                             ->first();
+            //             if(isset($similaritas)){
+            //                 if($similaritas->cosine1 >= 0.7){
+            //                     $nilai_konversi = 2;
+            //                 }else if($similaritas->cosine1 >= 0.4){
+            //                     $nilai_konversi = 1;
+            //                 }else if($similaritas->cosine1 >= 0.1){
+            //                     $nilai_konversi = 0.5;
+            //                 }else{
+            //                     $nilai_konversi = 0;
+            //                 }
 
-                            if($similaritas->cosine2 >= 0.7){
-                                $nilai_konversiqe = 2;
-                            }else if($similaritas->cosine2 >= 0.4){
-                                $nilai_konversiqe = 1;
-                            }else if($similaritas->cosine2 >= 0.1){
-                                $nilai_konversiqe = 0.5;
-                            }else{
-                                $nilai_konversiqe = 0;
-                            }
+            //                 if($similaritas->cosine2 >= 0.7){
+            //                     $nilai_konversiqe = 2;
+            //                 }else if($similaritas->cosine2 >= 0.4){
+            //                     $nilai_konversiqe = 1;
+            //                 }else if($similaritas->cosine2 >= 0.1){
+            //                     $nilai_konversiqe = 0.5;
+            //                 }else{
+            //                     $nilai_konversiqe = 0;
+            //                 }
 
-                            $data = [
-                                'id_jawaban'          => $jawaban->id_jawaban,
-                                'id_soal'          => $id_soal,
-                                'nilai_similaritas'   => $similaritas->cosine1,
-                                'nilai_similaritasqe' => $similaritas->cosine2,
-                                'nilai_sistem'        => $similaritas->cosine2,
-                                'nilai_konversi'        => $nilai_konversi,
-                                'nilai_konversiqe'        => $nilai_konversiqe
-                            ];
-                            $sim = Similarity::create($data);
-                            if($sim){
-                                echo 'similarity ok, ';
-                            }
-                            $total_nilai[] = $similaritas->cosine1;
-                            $total_nilaiqe[] = $similaritas->cosine2;
-                        }else{
-                            dd('API Offline');
-                        }
-                        $count++;
-                    }catch(QueryException $e){
-                        dd($e->errorInfo);
-                    }            
-                }
+            //                 $data = [
+            //                     'id_jawaban'          => $jawaban->id_jawaban,
+            //                     'id_soal'          => $id_soal,
+            //                     'nilai_similaritas'   => $similaritas->cosine1,
+            //                     'nilai_similaritasqe' => $similaritas->cosine2,
+            //                     'nilai_sistem'        => $similaritas->cosine2,
+            //                     'nilai_konversi'        => $nilai_konversi,
+            //                     'nilai_konversiqe'        => $nilai_konversiqe
+            //                 ];
+            //                 $sim = Similarity::create($data);
+            //                 if($sim){
+            //                     echo 'similarity ok, ';
+            //                 }
+            //                 $total_nilai[] = $similaritas->cosine1;
+            //                 $total_nilaiqe[] = $similaritas->cosine2;
+            //             }else{
+            //                 dd('API Offline');
+            //             }
+            //             $count++;
+            //         }catch(QueryException $e){
+            //             dd($e->errorInfo);
+            //         }            
+            //     }
 
-                // Hitung total nilai
-                try{
-                    $data = [
-                        'id_ujian' => $this->id_ujian,
-                        'id_siswa' => $id_siswa,
-                        'total_nilai' => array_sum($total_nilai)/$jumlah_soal,
-                        'total_nilaiqe' => array_sum($total_nilaiqe)/$jumlah_soal
-                    ];
-                    $sco = Score::create($data);
-                    if($sco){
-                        echo 'score ok, jumlah_soal: '.$jumlah_soal;
+            //     // Hitung total nilai
+            //     try{
+            //         $data = [
+            //             'id_ujian' => $this->id_ujian,
+            //             'id_siswa' => $id_siswa,
+            //             'total_nilai' => array_sum($total_nilai)/$jumlah_soal,
+            //             'total_nilaiqe' => array_sum($total_nilaiqe)/$jumlah_soal
+            //         ];
+            //         $sco = Score::create($data);
+            //         if($sco){
+            //             echo 'score ok, jumlah_soal: '.$jumlah_soal;
                     
-                    }
-                }catch(QueryException $e){
-                    dd($e->errorInfo);
-                }
-            }else{
-                echo 'not found ';
-            }    
+            //         }
+            //     }catch(QueryException $e){
+            //         dd($e->errorInfo);
+            //     }
+            // }else{
+            //     echo 'not found ';
+            // }    
             
             echo 'ok done<br>';
             $count++;
